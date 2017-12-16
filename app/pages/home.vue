783a9b54a51081c9abca72d131dc43b10847c827
Charlemagne KOKODOKO <charlextra93@gmail.com>
2017-12-16 10:41:59 +0000

    Pose de l première pierre
    Commit d'initialisation.

---
+++ app/pages/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <f7-page>
+    <f7-navbar :title="$root.config.title" />
+    <f7-block inner inset>{{$lang('text')}}</f7-block>
+    <f7-block>
+      <f7-button big fill raised bg="green" href="https://github.com/scriptPilot/app-framework/blob/master/DOCUMENTATION.md" external>Documentation</f7-button>
+    </f7-block>
+  </f7-page>
+</template>
